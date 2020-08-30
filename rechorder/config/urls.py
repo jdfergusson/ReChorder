adc4013e3cc96cae4c126ebdfa1e40d9c30206ce
Joel <joel@fergfamily.co.uk>
2020-08-30 13:30:54 +0100

    Removed one sub-url

--- rechorder/config/urls.py
+++ rechorder/config/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^rechorder/', include('rechorder.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('rechorder.urls')),
 ]
