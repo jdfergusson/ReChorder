53f25d270ba7d56a447516a82aef956bf75e43db
Joel <joel@fergfamily.co.uk>
2021-09-18 16:11:27 +0100

    Pulled song page movement into common functionality

--- rechorder/static/rechorder/js/common.js
+++ rechorder/static/rechorder/js/common.js
@@ -7,3 +7,41 @@ function pC(c) {
 function n2c(n) {
     return key_names[n % 12];
 }
+
+function processKeyPress(e) {
+    e = e || window.event;
+
+    /* Scroll by 1/2 window height each button press */
+    var n = window.innerHeight / 2;
+
+    if (e.key == 'PageUp' || e.key == 'ArrowUp') {
+        e.preventDefault();
+        if (window.scrollY <= 5)
+        {
+            if (typeof prev == "function")
+            {
+                prev();
+            }
+        }
+        else
+        {
+            new_scroll_loc = document.body.scrollTop - n;
+            $("html, body").animate({ scrollTop: new_scroll_loc });
+        }
+    }
+    else if (e.key == 'PageDown' || e.key == 'ArrowDown') {
+        e.preventDefault();
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 5)
+        {
+            if (typeof next == "function")
+            {
+                next();
+            }
+        }
+        else
+        {
+            new_scroll_loc = document.body.scrollTop + n;
+            $("html, body").animate({ scrollTop: new_scroll_loc });
+        }
+    }
+}
