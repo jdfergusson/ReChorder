eada8a2c7d04ee501665027b1f01d4ba0cc02d8c
Joel <joel.fergusson@gmail.com>
2022-01-01 18:13:06 +0000

    Added robots.txt to disallow all scraping (#71)
    Co-authored-by: Joel <joel@fergfamily.co.uk>

--- rechorder/config/urls.py
+++ rechorder/config/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('rechorder.urls')),
+    url(r'robots.txt', TemplateView.as_view(template_name="rechorder/robots.txt", content_type="text/plain"))
 ]
