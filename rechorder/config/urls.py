631ec6003fdbbbb338781faf290728a36477e986
Joel Fergusson <joel@fergfamily.co.uk>
2022-04-15 13:52:42 +0100

    Updates to get things working with Django 4

--- rechorder/config/urls.py
+++ rechorder/config/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('rechorder.urls')),
-    url(r'robots.txt', TemplateView.as_view(template_name="rechorder/robots.txt", content_type="text/plain"))
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^', include('rechorder.urls')),
+    re_path(r'robots.txt', TemplateView.as_view(template_name="rechorder/robots.txt", content_type="text/plain"))
 ]
